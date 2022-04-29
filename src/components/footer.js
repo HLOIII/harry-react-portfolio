f5c17810c297fa17574020f5f33cf7233868ce2a
Harry <chairmanharryogletree@gmail.com>
2022-04-29 13:23:34 -0400

    <implemented footer section>

---
+++ src/components/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div class="footer" id="footer">
+            <a href="https://github.com/HLOIII" target="_blank"><img src="https://img.icons8.com/color/48/000000/github-2.png" alt="Github" className="icon" /></a>
+            <a href="https://www.linkedin.com/in/harry-ogletree-536bb6154/" target="_blank"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" className="icon" /></a>
+            <a href="n/a" target="_blank"><img src="https://img.icons8.com/color/48/000000/twitter.png" alt="Twitter" className="icon" /></a>
+            <p>
+                &copy; Harry Ogletree  |  <a href="" className="link">download resume</a>  |  <a href="https://coolors.co/f06543-e8e9eb-e0dfd5-313638-f09d51">color palette</a>  |  made with <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React" />
+            </p>
+
+        </div>
+    )
+}
+
+export default Footer;
