92d777308ff8419417a775c6b50bc4ff0bd57a93
Harry <chairmanharryogletree@gmail.com>
2022-04-29 16:11:28 -0400

    Updates

---
+++ precache-manifest.93947fcb1666484ef02ba6137faccabb.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543bb1bdf2c259cff847248d0602ace9",
+    "url": "/photo-port/index.html"
+  },
+  {
+    "revision": "80f08ad86a4fa8dee0e6",
+    "url": "/photo-port/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "4179a75c9ba6b4cdf317",
+    "url": "/photo-port/static/css/main.f4ae26b2.chunk.css"
+  },
+  {
+    "revision": "80f08ad86a4fa8dee0e6",
+    "url": "/photo-port/static/js/2.c9b89e5b.chunk.js"
+  },
+  {
+    "revision": "bdcfdbef37b771299e3e4252a0d10375",
+    "url": "/photo-port/static/js/2.c9b89e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4179a75c9ba6b4cdf317",
+    "url": "/photo-port/static/js/main.2ff68c6e.chunk.js"
+  },
+  {
+    "revision": "aa0e969ea7cf66d801c8",
+    "url": "/photo-port/static/js/runtime-main.75070f25.js"
+  },
+  {
+    "revision": "30e1f88a20db62442cc05afae4646bee",
+    "url": "/photo-port/static/media/abstract.30e1f88a.jpg"
+  },
+  {
+    "revision": "627817fba3ffb05d812bfc3f46822c79",
+    "url": "/photo-port/static/media/avatar.627817fb.png"
+  },
+  {
+    "revision": "4bb62f3c2a6c2b09fc75d23eaa74fcfb",
+    "url": "/photo-port/static/media/bootstrap-logo.4bb62f3c.png"
+  },
+  {
+    "revision": "741cdc4c703f4b7349461c22b74394cf",
+    "url": "/photo-port/static/media/budget-tracker.741cdc4c.PNG"
+  },
+  {
+    "revision": "c021342399a7d0e94ce6363fb8d8188e",
+    "url": "/photo-port/static/media/code.c0213423.png"
+  },
+  {
+    "revision": "b31f21dc2c9a4a4aa4960c5374a57049",
+    "url": "/photo-port/static/media/horiseon.b31f21dc.PNG"
+  },
+  {
+    "revision": "379fe880d7638d8d5e6493d9dcfac94c",
+    "url": "/photo-port/static/media/jquery-logo.379fe880.png"
+  },
+  {
+    "revision": "86695d5d2f75c08615e5db9c648e099f",
+    "url": "/photo-port/static/media/logo.86695d5d.png"
+  },
+  {
+    "revision": "8967ca67bbedb067ba0aedd74a407295",
+    "url": "/photo-port/static/media/mern-logo.8967ca67.png"
+  },
+  {
+    "revision": "a31475c1dc56753d95ae716fcc351810",
+    "url": "/photo-port/static/media/note-taker.a31475c1.PNG"
+  },
+  {
+    "revision": "c9bd530a182d978224679c27c4be16b9",
+    "url": "/photo-port/static/media/password.c9bd530a.PNG"
+  },
+  {
+    "revision": "19080b3cc1962fff3d289ffd360e640d",
+    "url": "/photo-port/static/media/redux-logo.19080b3c.png"
+  },
+  {
+    "revision": "6b86fefeceafa4264db6182ef6c009e7",
+    "url": "/photo-port/static/media/weather-dash.6b86fefe.PNG"
+  },
+  {
+    "revision": "f7099627bd7c464de1c0b40abf87ad86",
+    "url": "/photo-port/static/media/webpack-logo.f7099627.png"
+  }
+]);
