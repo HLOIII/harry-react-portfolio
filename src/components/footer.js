848acd99b407b1eb0954c1bfeae79ea116b6b551
Harry <chairmanharryogletree@gmail.com>
2022-04-29 13:36:13 -0400

    <added navigation bar with links>

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ function Footer() {
             <a href="https://www.linkedin.com/in/harry-ogletree-536bb6154/" target="_blank"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" className="icon" /></a>
             <a href="n/a" target="_blank"><img src="https://img.icons8.com/color/48/000000/twitter.png" alt="Twitter" className="icon" /></a>
             <p>
-                &copy; Harry Ogletree  |  <a href="" className="link">download resume</a>  |  <a href="https://coolors.co/f06543-e8e9eb-e0dfd5-313638-f09d51">color palette</a>  |  made with <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React" />
+                &copy; Harry Ogletree  |  <a href="https://github.com/HLOIII/harry-react-portfolio/blob/main/src/assets/Harry-Ogletree.pdf" className="link">download resume</a>  |  <a href="https://coolors.co/f06543-e8e9eb-e0dfd5-313638-f09d51">color palette</a>  |  made with <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React" />
             </p>
 
         </div>
