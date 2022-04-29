e3f5c22652c602bdd97a2ccdf051bd2baa811d19
Harry <chairmanharryogletree@gmail.com>
2022-04-29 16:14:34 -0400

    <Deploy the Website>

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { HashRouter as Router} from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import ParticlesBg from "particles-bg";
 
@@ -12,7 +12,7 @@ import ParticlesBg from "particles-bg";
 ReactDOM.render(
     <Router>
         <App />
-        <ParticlesBg type="square" bg={true}/>
+
     </Router>,
     document.getElementById('root')
 );
