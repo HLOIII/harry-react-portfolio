218eaf0b94e3097f1733511bb757a8ca1c37e409
Harry <chairmanharryogletree@gmail.com>
2022-04-30 10:45:54 -0400

    Updates

---
+++ precache-manifest.c8ff4ac61fb51f7734bd2f73974aa75a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23202f72d408a558c326d369651131d8",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "0f838f1cedefe857f651",
+    "url": "/react-portfolio/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "1547fc2c77bd9c07564d",
+    "url": "/react-portfolio/static/css/main.fa98d013.chunk.css"
+  },
+  {
+    "revision": "0f838f1cedefe857f651",
+    "url": "/react-portfolio/static/js/2.3a8a1283.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.3a8a1283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1547fc2c77bd9c07564d",
+    "url": "/react-portfolio/static/js/main.d3292982.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "30e1f88a20db62442cc05afae4646bee",
+    "url": "/react-portfolio/static/media/abstract.30e1f88a.jpg"
+  },
+  {
+    "revision": "627817fba3ffb05d812bfc3f46822c79",
+    "url": "/react-portfolio/static/media/avatar.627817fb.png"
+  },
+  {
+    "revision": "4bb62f3c2a6c2b09fc75d23eaa74fcfb",
+    "url": "/react-portfolio/static/media/bootstrap-logo.4bb62f3c.png"
+  },
+  {
+    "revision": "741cdc4c703f4b7349461c22b74394cf",
+    "url": "/react-portfolio/static/media/budget-tracker.741cdc4c.PNG"
+  },
+  {
+    "revision": "c021342399a7d0e94ce6363fb8d8188e",
+    "url": "/react-portfolio/static/media/code.c0213423.png"
+  },
+  {
+    "revision": "b31f21dc2c9a4a4aa4960c5374a57049",
+    "url": "/react-portfolio/static/media/horiseon.b31f21dc.PNG"
+  },
+  {
+    "revision": "379fe880d7638d8d5e6493d9dcfac94c",
+    "url": "/react-portfolio/static/media/jquery-logo.379fe880.png"
+  },
+  {
+    "revision": "86695d5d2f75c08615e5db9c648e099f",
+    "url": "/react-portfolio/static/media/logo.86695d5d.png"
+  },
+  {
+    "revision": "8967ca67bbedb067ba0aedd74a407295",
+    "url": "/react-portfolio/static/media/mern-logo.8967ca67.png"
+  },
+  {
+    "revision": "a31475c1dc56753d95ae716fcc351810",
+    "url": "/react-portfolio/static/media/note-taker.a31475c1.PNG"
+  },
+  {
+    "revision": "c9bd530a182d978224679c27c4be16b9",
+    "url": "/react-portfolio/static/media/password.c9bd530a.PNG"
+  },
+  {
+    "revision": "19080b3cc1962fff3d289ffd360e640d",
+    "url": "/react-portfolio/static/media/redux-logo.19080b3c.png"
+  },
+  {
+    "revision": "6b86fefeceafa4264db6182ef6c009e7",
+    "url": "/react-portfolio/static/media/weather-dash.6b86fefe.PNG"
+  },
+  {
+    "revision": "f7099627bd7c464de1c0b40abf87ad86",
+    "url": "/react-portfolio/static/media/webpack-logo.f7099627.png"
+  }
+]);
